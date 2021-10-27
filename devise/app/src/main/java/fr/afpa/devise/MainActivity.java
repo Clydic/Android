8ed53bde85f419fcf32066ee951a853c82851dbb
Clydic <70671047+Clydic@users.noreply.github.com>
2021-10-27 14:25:45 +0200

    feat : Affichage dynamique des devises et ajout d'une image de fond

--- devise/app/src/main/java/fr/afpa/devise/MainActivity.java
+++ devise/app/src/main/java/fr/afpa/devise/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import metier.Convert;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner sPinnerDepart;
@@ -18,50 +26,79 @@ public class MainActivity extends AppCompatActivity {
     private Button bConvert;
     private  Button bQuitt;
     private final static String TAG ="MainActivity";
-
+    private ArrayList<String> arrayOfKey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sPinnerDepart=findViewById(R.id.SpinnerDepart);
-        sPinnerArrived=findViewById(R.id.SpinnerArrivee);
         tMoney = findViewById(R.id.iMontant);
         bConvert = findViewById(R.id.bConvert);
         bQuitt = findViewById(R.id.bQuitt);
-
-
+        chargeDevises();
+        charcheSpinner(R.id.SpinnerArrivee);
+        charcheSpinner(R.id.SpinnerDepart);
     }
     public void onConvert(View v){
         // Log.i(TAG , "onConvert");
-        Toast.makeText(getBaseContext(),"onConvert", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getBaseContext(),"onConvert", Toast.LENGTH_SHORT).show();
+
+        // We initiate variables
         EditText eMontant = (EditText) findViewById(R.id.iMontant);
         String strMontant = eMontant.getText().toString();
-        Double dbMontant = Double.valueOf(strMontant);
+
         Spinner nbDepart = (Spinner) findViewById(R.id.SpinnerDepart);
         Spinner nbArrivee = (Spinner) findViewById(R.id.SpinnerArrivee);
         String dNbDepart = (String) nbDepart.getSelectedItem().toString();
         String dNbArrivee = (String) nbArrivee.getSelectedItem().toString();
-        if (dNbDepart == dNbArrivee)
+        // We check if fields are empty or in the wrong format
+        if(strMontant.equals("") || strMontant.matches("[^0-9]")|| dNbDepart.equals("")
+        || dNbArrivee.equals(""))
+        {
+            Toast.makeText(getBaseContext(), "Des champs sont vides et doivent être renseignées",
+                    Toast.LENGTH_LONG).show();
+        // We check if currency of start and end are not the same
+        }else if (dNbDepart.equals(dNbArrivee))
         {
             Toast.makeText(getBaseContext(), "Les devises sont identique",
                     Toast.LENGTH_LONG).show();
 
-        }else if(eMontant.equals(null)==false || dNbArrivee.equals(null)==false || dNbDepart.equals(null)==false)
-        {
-            Toast.makeText(getBaseContext(), "Les champs sont vides",
+        } else
+            // We put a try control to check if the field is a Number
+        {   try {
+
+            Double dbMontant = Double.valueOf(strMontant);
+            Double res = Convert.convertir(dNbDepart,dNbArrivee,dbMontant);
+            // Calculate the number given in the tatget currency
+            String msg = strMontant + " " + dNbDepart + " fait " + res + " " + dNbArrivee;
+            Toast.makeText(getBaseContext(), msg,
                     Toast.LENGTH_LONG).show();
-        }else
-        {
-            Log.i(TAG, "Tout va bien");
+        }catch (NumberFormatException e){
+            Toast.makeText(getBaseContext(), "Vous devez enter un Nombre ",
+                    Toast.LENGTH_LONG).show();
+        }
+
         }
 
 
 
 
+    }
+    protected void  chargeDevises(){
+        Map<String,Double>  tableau = Convert.getConversionTable();
+        this.arrayOfKey = new ArrayList<String>(tableau.keySet());
 
     }
+
+    protected void charcheSpinner(Integer idSpinner){
+        Spinner spinner = findViewById(idSpinner);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_spinner_item, this.arrayOfKey );
+        spinner.setAdapter(adapter);
+
+    }
+
     public void onQuitt(View v){
         Log.i(TAG , "onQuitt");
         Toast.makeText(getBaseContext(),"onQuitt", Toast.LENGTH_SHORT).show();
+        System.exit(0);
     }
 }
